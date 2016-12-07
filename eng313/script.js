0d971e76bfee44604ac9d7a7fa2b12c3823a1343
Sam Mallamaci <sam.mallamaci@gmail.com>
2016-12-07 09:23:48 -0500

    last push of website draft

---
+++ eng313/script.js
@@ -0,0 +1,16 @@
+function displayAbout(){
+  var aboutText = "<h1>Hello World, my name is Samuel Mallamaci.</h1>"+
+  "<p>I am seeking a software development internship position in which my"+
+  " technical skills and team-building skills will have the opportunity grow and be challenged.</p>"+
+  "<h1>About Me</h1>"+
+  "<p>I am currently a student at Miami University in Oxford Ohio.</br>My major"+
+  " is Computer Science, and currently I am interested in web development and all things linux. I enjoy attending hackathons;"+
+  "I attended HackMIT in September 2016"+
+  "(unfortunatley my team and I did not place), and recently I helped"+
+  "plan and organize my school's ACM Chapter's first hackathon for Local"+
+  "Hack Day.</p>";
+  document.getElementById("content").innerHTML = aboutText;
+}
+function displayEducation(){
+  var eduText =
+}
